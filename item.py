2a7a8db1ee0821b1c257b8f71e75bb4da79a0c4a
yczhangsjtu <yczhangsjtu@github.com>
2016-08-16 09:21:57 +0800

    Add figure

--- item.py
+++ item.py
@@ -13,11 +13,29 @@ class TextItem(Item):
         texts = self.text.split("\n")
         return "  " + "\n  ".join(texts) + "\n"
 
+class FigureItem(Item):
+
+    def __init__(self,path):
+        self.type = "figure"
+        self.path = path
+        self.scale = 1.0
+
+    def generate(self):
+        ret = ""
+        ret += "  \\begin{figure}\n"
+        ret += "    \\scalebox{%f}{\\includegraphics{%s}}\n"%(self.scale,self.path)
+        ret += "  \\end{figure}\n"
+        return ret
+
 class ItemLoader:
 
     def __init__(self,data):
         if data["type"] == "text":
             self.item = TextItem(data["text"])
+        elif data["type"] == "figure":
+            self.item = FigureItem(data["path"])
+            if "scale" in data:
+                self.item.scale = data["scale"]
         else:
             self.item = None
 
---
+++ visualizer.py
@@ -0,0 +1,76 @@
+#!/usr/bin/python2.6
+'''
+    JSONVisualizer
+    - A simple json tree visualizer
+'''
+__VERSION__ = '0.0.1'
+
+import wx
+import os.path, dircache
+
+class JSONVisualizer(wx.Frame):
+    def __init__(self, jsonObj, windowTitle, *args, **kwds):
+        kwds["style"] = wx.DEFAULT_FRAME_STYLE
+        wx.Frame.__init__(self, *args, **kwds)
+        self.tree = wx.TreeCtrl(self, -1, style=wx.TR_HAS_BUTTONS|wx.TR_DEFAULT_STYLE|wx.SUNKEN_BORDER)
+
+        sizer = wx.BoxSizer(wx.VERTICAL)
+        sizer.Add(self.tree, 1, wx.EXPAND, 0)
+        self.SetAutoLayout(True)
+        self.SetSizer(sizer)
+        sizer.Fit(self)
+        sizer.SetSizeHints(self)
+        self.Layout()
+        self.SetTitle(windowTitle)
+        self.jsonObj = jsonObj
+        self.rootID = self.tree.AddRoot('root')
+#        try:
+        self.buildJSONTree(jsonObj, self.rootID)
+#        except:
+#            pass
+#        self.tree.Expand(self.rootID)
+        self.SetSize((500,500))
+    def buildJSONTree(self, jsonObj, parentID, parentTitle = None):
+        if jsonObj == None:
+            self.tree.AppendItem(parentID, 'None')
+            return
+        if isinstance(jsonObj, dict):
+            for (k,v) in jsonObj.iteritems():
+                childID = self.tree.AppendItem(parentID, k.encode('utf8'))
+                self.buildJSONTree(v, childID, k.encode('utf8'))
+#                self.tree.Expand(childID)
+        elif isinstance(jsonObj, list):
+            for index,item in enumerate(jsonObj):
+                if parentTitle:
+                    childID = self.tree.AppendItem(parentID, '%s[%d]'%(parentTitle,index))
+                else:
+                    childID = self.tree.AppendItem(parentID, '[%d]'%(index))
+                self.buildJSONTree(item, childID)
+#                self.tree.Expand(childID)
+        else:
+            if isinstance(jsonObj, basestring):
+                jsonObj = jsonObj.encode('utf8')
+            self.tree.AppendItem(parentID, str(jsonObj))
+
+if __name__ == "__main__":
+    try:
+        import sys
+        from json import load
+        title = "JSONVisualizer - %s"
+        if len(sys.argv) > 1:
+            f = open(sys.argv[1], 'r')
+            title = title % sys.argv[1]
+        else:
+            f = sys.stdin
+            title = title % "stdin"
+        jsonObj = load(f)
+        f.close()
+        app = wx.PySimpleApp(0)
+        wx.InitAllImageHandlers()
+        frame = JSONVisualizer(jsonObj, title, None, -1, "")
+        app.SetTopWindow(frame)
+        frame.Show()
+        app.MainLoop()
+    except Exception as ex:
+        print ex
+        raise
