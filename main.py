d84be39bfd7ff104162a53425a78e3bd293c62ab
Yuncong Zhang <shjdzhangyuncong@sjtu.edu.cn>
2017-09-19 13:57:12 +0800

    Temp store.

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
+#!/usr/bin/python
+
 import json
+import sys
+
+sys.path.append('/Users/yuncong/Projects/BeamerGenerator')
+
 from beamer import BeamerLoader
 
 if __name__ == "__main__":
-    with open("test.json") as f:
-        text = f.read()
-    data = json.loads(text)
-    loader = BeamerLoader(data)
-    beamer = loader.beamer
+	filename = "test.json"
+	if len(sys.argv) > 1:
+		filename = sys.argv[1]
+	with open(filename) as f:
+			text = f.read()
+	data = json.loads(text)
+	loader = BeamerLoader(data)
+	beamer = loader.beamer
 
-    print beamer.generate()
+	print beamer.generate()
